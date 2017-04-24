857a448c02fe5753e74905d18348c161d671926b
mario70pt <mario70pt@users.noreply.github.com>
2017-04-24 21:21:03 +0200

    pierwsza zmienna js

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+//pierwszy script.js !!!!!!
+
+var a = prompt('wprowadz dane podstawy trojkata a', '');
+var h = prompt('wprowadz wysokosc trojkata h', '');
+var triangleArea = a * h / 2;
+alert('Pole trojkąta ' + triangleArea);
+console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
